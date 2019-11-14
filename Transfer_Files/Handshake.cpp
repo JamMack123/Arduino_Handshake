3fb7998d466c2cf01d667af12d8db42c72c55ae9
J872 <57580342+J872@users.noreply.github.com>
2019-11-14 09:01:29 -0700

    Added possible one way communication

--- Transfer_Files/Handshake.cpp
+++ Transfer_Files/Handshake.cpp
@@ -3,17 +3,18 @@ uint32_t d = 0;
 uint32_t n = 0;
 uint32_t e = 0;
 uint32_t m = 0;
-int status = 0;
 
 void setup(){
     int val;
     init();
     Serial.begin(9600);
+    Serial3.begin(9600);
     pinMode(13, INPUT);
     digitalWrite(13, LOW);
     val = digitalRead(13);
     if (val == 1){
         Serial.println("Arduino chat: Server!");
+        // For some reason we need this delay IT IS SACRED CODE!
         delay(20);
         d = 27103;
         n = 95477;
@@ -23,6 +24,7 @@ void setup(){
     }
     if (val == 0){
         Serial.println("Arduino chat: Client!");
+        // For some reason we need this delay IT IS SACRED CODE!
         delay(20);
         d = 38291;
         n = 84823;
@@ -30,9 +32,21 @@ void setup(){
         m = 95477;
     }
 }
+void reader(){
+    char myArray[20];
+    if (Serial.available()>=1){
+        int numBytes =Serial.readBytes(myArray, 1);
+        for(int i = 0; i < numBytes; i++){
+            Serial.print(myArray[i]);
+            Serial3.print(myArray[i]);
+        }
+        Serial.println();
+    }
+}
 int main(){
     setup();
-    Serial.println();
-    Serial.print(e);
-    delay(10);
+   
+    while (true){
+        reader();
+    }
 }
