8abb4e77cde3d9096acc4b49e701124ec661f730
J872 <57580342+J872@users.noreply.github.com>
2019-11-14 08:45:27 -0700

    Updated Server keys

--- Transfer_Files/Handshake.cpp
+++ Transfer_Files/Handshake.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h>
-uint32_t serverPublicKey = 7;
-uint32_t serverPrivateKey = 27103;
-uint32_t serverModulus = 95477;
-uint32_t clientPublicKey = 11;
-uint32_t clientPrivateKey = 38291;
-uint32_t clientModulus = 84823;
+uint32_t d = 0;
+uint32_t n = 0;
+uint32_t e = 0;
+uint32_t m = 0;
 int status = 0;
 
 void setup(){
@@ -17,12 +15,24 @@ void setup(){
     if (val == 1){
         Serial.println("Arduino chat: Server!");
         delay(20);
+        d = 27103;
+        n = 95477;
+        e = 11;
+        m = 84823;
+   
     }
     if (val == 0){
         Serial.println("Arduino chat: Client!");
         delay(20);
+        d = 38291;
+        n = 84823;
+        e = 7;
+        m = 95477;
     }
 }
 int main(){
     setup();
+    Serial.println();
+    Serial.print(e);
+    delay(10);
 }
