662fc542d5cad80bb947ca733d5c3190bbac81d7
J872 <57580342+J872@users.noreply.github.com>
2019-11-14 10:06:37 -0700

    This may work

--- Transfer_Files/Handshake.cpp
+++ Transfer_Files/Handshake.cpp
@@ -36,31 +36,38 @@ void setup(){
 * and finishing with the most significant byte .
 */
 void uinnt32_to_serial3 (uint32_t num) {
-    Serial3 . write (( char ) ( num >> 0) ) ;
-    Serial3 . write (( char ) ( num >> 8) ) ;
-    Serial3 . write (( char ) ( num >> 16) ) ;
-    Serial3 . write (( char ) ( num >> 24) ) ;
+    Serial3.write (( char ) ( num >> 0) ) ;
+    Serial3.write (( char ) ( num >> 8) ) ;
+    Serial3.write (( char ) ( num >> 16) ) ;
+    Serial3.write (( char ) ( num >> 24) ) ;
 }
 /* * Reads an uint32_t from Serial3 , starting from the least - significant
 * and finishing with the most significant byte .
 */
 uint32_t uinnt32_from_serial3 () {
 uint32_t num = 0;
-num = num | (( uint32_t ) Serial3 . read () ) << 0;
-num = num | (( uint32_t ) Serial3 . read () ) << 8;
-num = num | (( uint32_t ) Serial3 . read () ) << 16;
-num = num | (( uint32_t ) Serial3 . read () ) << 24;
+num = num | (( uint32_t ) Serial3.read () ) << 0;
+num = num | (( uint32_t ) Serial3.read () ) << 8;
+num = num | (( uint32_t ) Serial3.read () ) << 16;
+num = num | (( uint32_t ) Serial3.read () ) << 24;
 return num ;
+}
+uint32_t encryption(uint32_t val){
+    uint32_t x = 
+
 }
 void reader(){
     char myArray[20];
     if (Serial.available()>0){
+        if (Serial.read() == 13){
+            Serial3.print("\n\r");
+        }
         uint32_t byte_read =Serial.read();
         uinnt32_to_serial3(byte_read);
 
     }
     if (Serial3.available() > 0) {
-      uinnt32_from_serial3();
+      Serial.write(uinnt32_from_serial3());
 }
 }
 
