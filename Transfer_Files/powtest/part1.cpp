8c1192be43496b74878656b176a9bf6315c6e320
Jack Opgenorth <55570002+Jopgen@users.noreply.github.com>
2019-11-14 11:10:30 -0700

    Add files via upload

---
+++ Transfer_Files/powtest/part1.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int mulmod(uint32_t a, uint32_t b, uint32_t m){
+	uint32_t sum = 0;
+	for (int i = 0; i < 31 ; i++){
+		if ( (a & 1<<i) != 0) {
+			sum = ((((1 << i) * b) % m) + sum) % m;
+		}
+	}
+
+	return  sum;
+
+}
+
+
+
+int main(){
+
+	uint32_t sPub = 7;
+	uint32_t sPriv = 27103;
+	uint32_t sm = 95477;
+	uint32_t cPub = 11;
+	uint32_t cPriv = 38291;
+	uint32_t cm = 84823;
+	
+
+
+
+	uint32_t k = 1864;
+	uint32_t p = 86569;
+	uint32_t o = 8757;
+	uint32_t c = 'a';
+
+	uint32_t pow = 97;
+
+	uint32_t ans = 1;
+
+
+	
+	for (int i = 0; i < 32; i++){
+		if ((cPub & 1 << i) != 0){
+			ans = mulmod(ans, pow, cm);
+		}
+		pow = mulmod(pow,pow,cm);
+	}
+	cout << ans << endl;
+
+
+	uint32_t pow2 = 69287;
+	uint32_t ans2 = 1;
+	for (int i = 0; i < 32; i++){
+		if ((cPriv & 1 << i) != 0){
+			ans2 = mulmod(ans2, pow2, cm);
+		}
+		pow = mulmod(pow2,pow2,cm);
+	}
+	cout << ans2 << endl;
+
+
+
+
+
+
+	return 0;
+
+
+
+}
